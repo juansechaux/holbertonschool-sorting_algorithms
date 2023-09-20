91d1deba327414f3c14d7b46b1160b07b4abb0a1
juansechaux <juansechaux@hotmail.com>
2023-09-20 14:11:45 -0700

    Task 2

---
+++ 2-selection_sort.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+
+/**
+ * selection_sort - function that sorts an array of integers
+ * @array: array of numbers to sort
+ * @size: size of the array
+ * Return: void
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	unsigned int i = 0, j = 1;
+	int least, aux, least_index;
+
+	while (i < size - 1)
+	{
+		least = array[i];
+		while (j < size)
+		{
+			if (least > array[j])
+			{
+				least_index = j;
+				least = array[j];
+			}
+			j++;
+		}
+		if (i != (unsigned int)least_index)
+		{
+			aux = array[i];
+			array[i] = least;
+			array[least_index] = aux;
+			print_array(array, size);
+		}
+		i++;
+		j = i + 1;
+	}
+
+}
