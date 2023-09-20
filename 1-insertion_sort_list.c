b245b8c27b3c7de57de5b394795349a53430a9b1
juansechaux <juansechaux@hotmail.com>
2023-09-20 10:08:02 -0700

    1-insertion_sort_list.c

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,40 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - function that sorts an array of integers
+ * @list: double linked list to sort
+ * Return: void
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *copyPrev, *copy;
+
+	if (!*list || !list)
+		return;
+
+	current = *list;
+
+	while ((current = current->next))
+	{
+		copy = current;
+
+		while (copy->prev && copy->n < copy->prev->n)
+		{
+			copyPrev = copy->prev;
+			if (copy->next)
+				copy->next->prev = copyPrev;
+			if (copyPrev->prev)
+				copyPrev->prev->next = copy;
+			else
+				*list = copy;
+
+			copyPrev->next = copy->next;
+			copy->prev = copyPrev->prev;
+			copy->next = copyPrev;
+			copyPrev->prev = copy;
+
+			print_list(*list);
+		}
+	}
+}
